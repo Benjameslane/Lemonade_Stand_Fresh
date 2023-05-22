b197392ec8a2db20f5ca18a3a956f6280531dc7f
Benjameslane <ben.james.lane9>
2023-05-21 21:07:56 -0600

    Added the SOLID method principles explanations

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -13,8 +13,9 @@ namespace LemonadeStand
 
             // S in SOLID is displayed within the day class under GetWeatherForecastToday()   
             // Another S in Solid is displayed within the Game class under DisplayWelcome()
-
-
+            // The Liskov substitution principle is shown within the LemonadePitcher child class.
+            // The Liskov substitution principle applies to the rest if Items child classes
+            // there are no examples of Interface Segregation Principles due to there being no methods within the Parent classes
         }
     }
 }
